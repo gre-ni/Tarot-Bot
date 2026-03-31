d3b9e59eab98468f8ac693472bf4f26eeb4ffdce
NikolG <greplova.nikol@gmail.com>
2026-03-31 23:53:46 +0200

    Setting up discord

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from get_tarot import get_random_card, parse_response, format_dict
-import json
 
 def main():
-    amount = 1 # As MVP I am only getting one random card
-    [selected_card] = parse_response(get_random_card(amount)) # Unpacking list with only one card dict
+    # As MVP I am only getting one random card, function default, eventually prompting for multiple?
+    [selected_card] = parse_response(get_random_card()) # Unpacking list with only one card dict
 
-    print(format_dict(selected_card))
+    return format_dict(selected_card)
 
 
 if __name__ == "__main__":
